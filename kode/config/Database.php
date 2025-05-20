3d185c03baa23fc4d5b39f232c1e5fdda6323fe3
yazidmadarizel <69192961+yazidmadarizel@users.noreply.github.com>
2025-05-20 15:59:09 +0700

    Update Database.php

--- kode/config/Database.php
+++ kode/config/Database.php
@@ -4,21 +4,12 @@ class Database {
     private $username = "root";
     private $password = "";
     private $database = "restaurant";
-    private $port = 3307; 
     private $conn;
-
-    public function __construct($port = 3307) {
-        $this->port = $port;
-    }
 
     public function getConnection() {
         $this->conn = null;
         try {
-            $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->database,
-                $this->username,
-                $this->password
-            );
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
             echo "Connection error: " . $e->getMessage();
